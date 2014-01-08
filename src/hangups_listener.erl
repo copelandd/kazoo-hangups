171e5daabbfae52351331f36c31322b769d3d311
karl anderson <karl@2600hz.com>
2014-01-08 18:25:33 -0500

    KAZOO-1539: started work on wh_amqp_assignments (new mechanism for associating channels with Kazoo processes)

--- src/hangups_listener.erl
+++ src/hangups_listener.erl
@@ -12,9 +12,7 @@
 
 -behaviour(gen_listener).
 
--export([start_link/0
-         ,handle_cdr/2
-        ]).
+-export([start_link/0]).
 -export([init/1
          ,handle_call/3
          ,handle_cast/2
